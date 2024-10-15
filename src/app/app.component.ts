6d54e9a54f1f7b8b3d9b4109b59f6b22abb8e847
Kevyn Giron <kgprogrammer98@gmail.com>
2024-10-14 19:00:42 -0600

    Punto numero dos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 	constructor(
         public service: AppService,
 	) { }
-	
+
 	ngOnInit() {
 		this.obtenerTareas();
 	}
@@ -21,4 +21,17 @@ export class AppComponent {
 	async obtenerTareas() {
 		this.tareas = await this.service.obtenerTareas();
 	}
+
+  nuevaTarea = {
+    titulo: '',
+    minutos: 0
+  }
+
+  agregarTarea() {
+    if (this.nuevaTarea.titulo && this.nuevaTarea.minutos) {
+      this.tareas.push({ ...this.nuevaTarea, id: this.tareas.length + 1 });
+      this.nuevaTarea.titulo = '';
+      this.nuevaTarea.minutos = 0;
+    }
+  }
 }
