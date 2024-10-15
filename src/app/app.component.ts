e2f11ef66d99ae6db3245574b8fd1135383677ea
Kevyn Giron <kgprogrammer98@gmail.com>
2024-10-14 19:15:02 -0600

    Punto numero tres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,11 +27,18 @@ export class AppComponent {
     minutos: 0
   }
 
+  //metodo para agregar una tarea
   agregarTarea() {
     if (this.nuevaTarea.titulo && this.nuevaTarea.minutos) {
-      this.tareas.push({ ...this.nuevaTarea, id: this.tareas.length + 1 });
+      this.tareas.push({ ...this.nuevaTarea, id: this.tareas.length + 1, seleccionado: false });
       this.nuevaTarea.titulo = '';
       this.nuevaTarea.minutos = 0;
     }
+  }
+
+  //metodo para manejar elementos seleccionados
+  procesarElementosSeleccionados() {
+    const seleccionados = this.tareas.filter(elemento => elemento.seleccionado);
+    //hacer algo aqui despues
   }
 }
